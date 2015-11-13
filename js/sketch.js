8af66db71d68e54969507db845c06a93c4e6184a
sle7 <sle7git@gmail.com>
2015-11-12 22:06:40 -0800

    hover effect
    added mouse over coloring: solid black

--- js/sketch.js
+++ js/sketch.js
@@ -6,15 +6,18 @@ var boxWidth = 16;
 
 $(document).ready(function(){
     loadBox(boxWidth);
+    $('.box').hover(function(){
+        $(this).css('background-color', 'black');
+    });
 });
 
 function loadBox(size){
     for(var i = 0; i < size*size; i++){
         if(i % 16 === 0){
-            $('.wrapper').append("<div class='endBox'>"+i+"</div>");
+            $('.wrapper').append("<div class='box' class='endBox'></div>");
         }
         else{
-            $('.wrapper').append("<div class='box'>"+i+"</div>");
+            $('.wrapper').append("<div class='box'></div>");
         }
     }
 }
