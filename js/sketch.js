41345bcd0c5a1c6e3997de79248b4139c786cfe8
sle7 <sle7git@gmail.com>
2015-11-24 20:37:40 -0800

    add random color feature

--- js/sketch.js
+++ js/sketch.js
@@ -2,23 +2,35 @@
  * Created by Admin on 11/11/2015.
  */
 
-var boxWidth = 16;
-
+var start = 16;
+var baseColor = "black";
 $(document).ready(function(){
-    loadBox(boxWidth);
-    $('.box').hover(function(){
-        $(this).css('background-color', 'black');
+    loadBox(start);
+    $('#clearpad').click(function(){
+        $('.box').css('background-color', 'transparent');
+        var side = prompt("How many squares per side?");
+        loadBox(side);
+    });
+    $("#random").click(function(){
+        baseColor = "#"+(Math.random()*0xFFFFFF<<0).toString(16);
     });
 });
 
 function loadBox(size){
+    $('.wrapper').empty();
     for(var i = 0; i < size*size; i++){
-        if(i % 16 === 0){
+        if(i % size === 0){
             $('.wrapper').append("<div class='box' class='endBox'></div>");
         }
         else{
             $('.wrapper').append("<div class='box'></div>");
         }
     }
+    $('.box').css('background-color', "grey");
+    $('.box').css('height', 100 / size + '%');
+    $('.box').css('width', 100 / size + '%');
+    $('.box').hover(function(){
+        $(this).css('background-color', baseColor);
+    });
 }
 
